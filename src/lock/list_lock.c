c096ced14acee01dfb87892eae256302ee0daf37
lss525 <1282852660@qq.com>
2026-04-14 18:06:02 +0800

    list

--- src/lock/list_lock.c
+++ src/lock/list_lock.c
@@ -1,20 +1,50 @@
 #include "list_lock.h"
-
 #include <stdio.h>
 #include <stdlib.h>
 
-void listInit(list_lock_t* list) {
-   perror("This function is not implemented");
+void listInit(list_lock_t* list){
+    list->head = NULL;
+    pthread_mutex_init(&list->mutex, NULL);
+    pthread_cond_init(&list->cond, NULL);
 }
 
-void producer(list_lock_t* list, DataType value) {
-  perror("This function is not implemented");
+void producer(list_lock_t* list, DataType value){
+    LinkList newNode = (LinkList)malloc(sizeof(LNode));
+    newNode->value = value;
+    
+    pthread_mutex_lock(&list->mutex);
+    
+    newNode->next = list->head;
+    list->head = newNode;
+    
+    pthread_cond_signal(&list->cond);
+    pthread_mutex_unlock(&list->mutex);
 }
 
-void consumer(list_lock_t* list) {
-   perror("This function is not implemented");
+void consumer(list_lock_t* list){
+    pthread_mutex_lock(&list->mutex);
+    
+    while(list->head == NULL){
+        pthread_cond_wait(&list->cond, &list->mutex);
+    }
+    
+    LinkList temp = list->head;
+    list->head = list->head->next;
+    free(temp);
+    
+    pthread_mutex_unlock(&list->mutex);
 }
 
-int getListSize(list_lock_t* list) {
-  perror("This function is not implemented");
+int getListSize(list_lock_t* list){
+    pthread_mutex_lock(&list->mutex);
+    
+    int count = 0;
+    LinkList p = list->head;
+    while(p != NULL){
+        count++;
+        p = p->next;
+    }
+    
+    pthread_mutex_unlock(&list->mutex);
+    return count;
 }
--- src/lock/lock.c
+++ src/lock/lock.c
@@ -1,15 +1,68 @@
 #include "lock.h"
-
 #include <stdio.h>
+#include <pthread.h>
 
-void amountInit(lock_t* Account) {
-  perror("This function is not implemented");
+void amountInit(lock_t* account) {
+    if (account == NULL) {
+        perror("Invalid account pointer");
+        return;
+    }
+    
+    if (pthread_mutex_init(&account->mutex, NULL) != 0) {
+        perror("Mutex initialization failed");
+        return;
+    }
+    
+    account->amount = 0;
 }
 
-void Income(lock_t* Account, int amount) {
-  perror("This function is not implemented");
+void Income(lock_t* account, int amount) {
+    if (account == NULL) {
+        perror("Invalid account pointer");
+        return;
+    }
+    
+    if (amount <= 0) {
+        perror("Invalid income amount");
+        return;
+    }
+    
+    if (pthread_mutex_lock(&account->mutex) != 0) {
+        perror("Mutex lock failed");
+        return;
+    }
+    
+    account->amount += amount;
+
+    if (pthread_mutex_unlock(&account->mutex) != 0) {
+        perror("Mutex unlock failed");
+    }
 }
 
-void Expend(lock_t* Account, int amount) {
-  perror("This function is not implemented");
+void Expend(lock_t* account, int amount) {
+    if (account == NULL) {
+        perror("Invalid account pointer");
+        return;
+    }
+    
+    if (amount <= 0) {
+        perror("Invalid expend amount");
+        return;
+    }
+    
+    if (pthread_mutex_lock(&account->mutex) != 0) {
+        perror("Mutex lock failed");
+        return;
+    }
+    
+    if (account->amount >= amount) {
+        account->amount -= amount;
+    } 
+    else {
+        perror("Insufficient balance");
+    }
+    
+    if (pthread_mutex_unlock(&account->mutex) != 0) {
+        perror("Mutex unlock failed");
+    }
 }
